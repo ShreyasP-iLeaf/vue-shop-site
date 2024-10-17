f57ce472d7edd89708c882426b0f403d5ba47f3f
ShreyasP-iLeaf <shreyas.p@ileafsolutions.com>
2024-10-17 19:14:20 +0530

    Completed header and footer section of the page

--- src/App.vue
+++ src/App.vue
@@ -1,91 +1,38 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
-<template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+<template class="max-w-[1440px]">
+  <NavSection position="header" :appWidth="appWidth" />
+  <div class="h-[150vh]"></div>
+  <NavSection position="footer" :appWidth="appWidth" />
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+<script>
+import NavSection from './components/common/NavSection.vue'
+
+export default {
+  data() {
+    return {
+      browserWidth: null,
+    }
+  },
+  methods: {
+    onResize(event) {
+      this.browserWidth = event.target.innerWidth
+    },
+  },
+  computed: {
+    appWidth() {
+      return this.browserWidth
+    },
+  },
+  components: {
+    NavSection: NavSection,
+  },
+  created() {
+    window.addEventListener('resize', this.onResize)
+  },
+  unmounted() {
+    window.removeEventListener('resize', this.onResize)
+  },
 }
+</script>
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
+<style scoped lang="scss"></style>
