20094a4d8ef43b270058e43589c0e4e5cda93f23
ShreyasP-iLeaf <shreyas.p@ileafsolutions.com>
2024-10-20 16:33:42 +0530

    Added cart in mobile screens and footer to product details page

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -9,20 +9,41 @@
   >
     <div
       v-if="product"
-      class="bg-black relative lg:flex lg:h-[calc(100vh-120px)] lg:justify-center lg:items-center p-5 md:p-10"
+      class="bg-black relative lg:flex lg:h-[calc(100vh-120px)] lg:justify-center lg:items-center p-5 md:p-10 mb-10"
     >
       <div
-        class="text-white font-bold text-2xl self-start w-full lg:hidden block lg:-top-0 left-5"
+        class="text-white flex justify-between items-center font-bold text-2xl self-start w-full lg:hidden lg:-top-0 left-5"
       >
-        <router-link
-          :to="{
-            name: 'home',
-          }"
-          class="cursor-pointer w-3 p-3 pl-0"
-          >Our Products</router-link
-        >
-        <div class="inline-block cursor-pointer w-3">/</div>
-        <div class="inline-block cursor-pointer w-3 p-3">Detail</div>
+        <div>
+          <router-link
+            :to="{
+              name: 'home',
+            }"
+            class="cursor-pointer w-3 p-3 pl-0"
+            >Our Products</router-link
+          >
+          <div class="inline-block cursor-pointer w-3">/</div>
+          <div class="inline-block cursor-pointer w-3 p-3">Detail</div>
+        </div>
+        <button class="relative h-[30px]">
+          <div
+            :class="`md:hidden text-sm absolute right-0 -top-3 rounded-full bg-[#e8eaed] text-black w-5 h-5 ${cartValue ? 'animate-pulse' : ''}`"
+          >
+            {{ cartValue }}
+          </div>
+          <svg
+            :class="`md:hidden ${cartValue ? 'animate-pulse' : ''}`"
+            xmlns="http://www.w3.org/2000/svg"
+            height="30px"
+            viewBox="0 -960 960 960"
+            width="30px"
+            fill="#e8eaed"
+          >
+            <path
+              d="M280-80q-33 0-56.5-23.5T200-160q0-33 23.5-56.5T280-240q33 0 56.5 23.5T360-160q0 33-23.5 56.5T280-80Zm400 0q-33 0-56.5-23.5T600-160q0-33 23.5-56.5T680-240q33 0 56.5 23.5T760-160q0 33-23.5 56.5T680-80ZM246-720l96 200h280l110-200H246Zm-38-80h590q23 0 35 20.5t1 41.5L692-482q-11 20-29.5 31T622-440H324l-44 80h480v80H280q-45 0-68-39.5t-2-78.5l54-98-144-304H40v-80h130l38 80Zm134 280h280-280Z"
+            />
+          </svg>
+        </button>
       </div>
       <div
         :class="`relative max-w-7xl h-auto grid grid-cols-1 lg:grid-cols-2 bg-white`"
@@ -78,7 +99,7 @@
             <button
               v-if="ifAtleastOneItem(product) === -1"
               @click="addProductToCart(product)"
-              class="h-[52px] inline-block px-3 py-2 bg-black text-white text-xl self-center"
+              class="h-[52px] inline-block px-3 py-2 max-w-[200px] bg-black text-white text-xl self-center"
             >
               Add to cart
             </button>
@@ -101,17 +122,21 @@
       </div>
     </div>
   </transition>
+  <NavSection v-show="product" position="footer" />
 </template>
 
 <script>
 import { cartStore } from '@/stores/app'
-
+import NavSection from '@/components/common/NavSection.vue'
 export default {
   name: 'ProductPage',
   data() {
     return {
       product: null,
     }
+  },
+  components: {
+    NavSection,
   },
   beforeMount() {
     fetch(`https://fakestoreapi.com/products/${this.$route.params.id}`)
@@ -124,6 +149,9 @@ export default {
   computed: {
     cartItems() {
       return cartStore().cart
+    },
+    cartValue() {
+      return cartStore().noOfItemsInCart
     },
   },
   methods: {
